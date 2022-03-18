0f8ac1ef8e0eab3243c517c3f186f8f1a1c8f1fd
Lionzj <31950799+Lionzj@users.noreply.github.com>
2022-03-18 10:56:53 +0800

    Update diy-part1.sh
    加回VSSR参数,添加argon配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,11 +29,12 @@ echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull origin master
 git branch --set-upstream-to=origin/master master
 
-# cd ../lean
-# git clone https://github.com/jerrykuku/lua-maxminddb.git
-# git clone https://github.com/jerrykuku/luci-app-vssr.git
+cd ../lean
+git clone https://github.com/jerrykuku/lua-maxminddb.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git
 
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/jerrykuku/luci-app-argon-config.git
 
 #git clone https://github.com/lisaac/luci-app-dockerman.git
