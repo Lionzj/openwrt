b93dbaa019b71ccfdf9f3fd873c3b41dfd9e8c0d
Lionzj <31950799+Lionzj@users.noreply.github.com>
2023-04-12 13:38:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,7 +28,7 @@ git init
 git remote add -f origin https://github.com/vernesong/OpenClash.git
 git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull origin master
+git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
 
 # cd ../lean
